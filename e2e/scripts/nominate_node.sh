7b3c62f0c4f921837b66a59be7f86ab71f6d3c1a
Jon Knight <jon@babble.io>
2019-06-06 14:07:21 +0100

    Add validation of parameters to the nominate_node.sh script. NB this has now moved to named parameters.
    The intention is to migrate all the other scripts to the same standard and level of validation.
    It is a background task, and will be largely be completed on trains.

--- e2e/scripts/nominate_node.sh
+++ e2e/scripts/nominate_node.sh
@@ -1,9 +1,59 @@
 #!/bin/bash
 
-FROMNODE=$1
-NOMINEENODE=$2
+
+
+usage()
+{
+    echo "usage: nominate_node.sh  [ --from NominatingNode --nominee NominatedNode | --help ]"
+}
+
+
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -f | --from )           shift
+                                FROMNODE=$1
+                                ;;
+        -n | --nominee )        shift
+                                NOMINEENODE=$1
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
+
+if [ -z "$FROMNODE" ] ; then
+	>&2 echo "You must specify the nominating node with the --from parameter. Aborting."
+	exit 1	
+fi
+
+if [ -z "$NOMINEENODE" ] ; then
+	>&2 echo "You must specify the nominated node with the --nominee parameter. Aborting."
+	exit 1	
+fi
+
 
 mydir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
+
+
+$mydir/check_docker_node_running.sh "$FROMNODE"
+if  [ $? -gt 0 ] ; then
+	>&2 echo "Aborting."
+        exit 1
+fi
+
+$mydir/check_docker_node_exists.sh "$NOMINEENODE"
+if  [ $? -gt 0 ] ; then
+	>&2 echo "Aborting."
+        exit 1
+fi
+
+#TODO check that $NOMINEENODE is exited and that the node has not been nominated aleady and that the $FROMNODE is authorised. 
 
 NOMADD=$(scripts/get_sleeping_node_address.sh $NOMINEENODE)
 FROMADD=$(scripts/get_sleeping_node_address.sh $FROMNODE)
