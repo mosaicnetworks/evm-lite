05b89b539bcfe62ea6ae5dfb797945b9f395a505
Jon Knight <jon@babble.io>
2019-06-19 12:29:44 +0100

    Manual merging of conflicts from poa and genesis

--- e2e/scripts/rebuild_ipsdat.sh
+++ e2e/scripts/rebuild_ipsdat.sh
@@ -6,7 +6,7 @@ mydir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 
 for node in $(docker ps --format "{{.Names}}" | sort -u)
 do
-  IP=$(bash $mydir/get_ip_of_container.sh  $node)	
+  IP=$(bash $mydir/get_node_ip.sh  $node)	
   echo "$node $IP"
 done > $mydir/../../deploy/terraform/local/ips.dat
 
